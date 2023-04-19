77ca764e01cd2f8b06cbadee550a63cf449f52db
Женя Шульман <jonnie.gray@yandex.ru>
2023-04-19 15:03:29 +0500

    fix: Внес мелкие правки в код касательно оформления.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -26,9 +26,9 @@ router_v1.register(
     CommentViewSet,
     basename='comments'
 )
-router_v1.register(r"categories", CategoryViewSet, basename="category")
-router_v1.register(r"genres", GenreViewSet, basename="genre")
-router_v1.register(r"titles", TitleViewSet, basename="title")
+router_v1.register(r'categories', CategoryViewSet, basename='category')
+router_v1.register(r'genres', GenreViewSet, basename='genre')
+router_v1.register(r'titles', TitleViewSet, basename='title')
 
 urlpatterns = [
     path("v1/", include(router_v1.urls)),
